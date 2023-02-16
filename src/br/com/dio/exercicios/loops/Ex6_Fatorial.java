bb34a895069e018b6135b38abc577152f3c17ec8
Luiz Roberto <luizrobertoao29@gmail.com>
2023-02-16 14:35:51 -0300

    feat: Resolvido Ex6_Fatorial.

--- src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
+++ src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
@@ -7,4 +7,34 @@ Faça um programa que calcule o fatorial de um número inteiro fornecido pelo us
 Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
 */
 public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+
+        System.out.println("Digite um número para o cálculo de fatorial:");
+        numero = sc.nextInt();
+
+
+// PRIMEIRA SOLUÇÃO:
+//        long resultadoFatorial = 0L;
+//        for (int i = 1; i < numero; i++) {
+//
+//            if (resultadoFatorial == 0) {
+//                resultadoFatorial = (long) numero * (numero - i);
+//            } else {
+//                resultadoFatorial = resultadoFatorial * (numero - i);
+//            }
+//
+//        }
+
+
+// SIMPLIFICANDO A SOLUÇÃO:
+        long resultadoFatorial = 1L;
+
+        for (int i = numero; i >= 1; i--) {
+            resultadoFatorial = resultadoFatorial * i;
+        }
+        System.out.println(numero + "! = " + resultadoFatorial);
+    }
 }
