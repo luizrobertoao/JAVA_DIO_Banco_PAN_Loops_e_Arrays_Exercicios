78177e22390f4e96dc8fb9b7182e636def3d8468
Luiz Roberto <luizrobertoao29@gmail.com>
2023-02-16 10:21:35 -0300

    feat: Resolvido Ex2_Nota.

--- src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -9,4 +9,39 @@ e continue pedindo
 até que o usuário informe um valor válido.
 */
 public class Ex2_Nota {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nota;
+
+//RESOLUÇÃO COM WHILE:
+        System.out.println("Digite uma nota: ");
+        nota = scanner.nextInt();
+
+        while (nota < 0 || nota > 10) {
+            System.out.println("Nota inválida, digite novamente: ");
+            nota = scanner.nextInt();
+        }
+
+
+//RESOLUÇÃO COM DO WHILE:
+
+//        boolean notaInvalida = true;
+//
+//        do {
+//            System.out.println("Digite uma nota: ");
+//            nota = scanner.nextInt();
+//            notaInvalida = (nota > 10) || (nota < 0);
+//
+//            if (notaInvalida) {
+//                System.out.println("Nota inválida!");
+//            } else {
+//                break;
+//            }
+//
+//        } while (notaInvalida);
+
+        System.out.println("Nota válida.");
+    }
 }
