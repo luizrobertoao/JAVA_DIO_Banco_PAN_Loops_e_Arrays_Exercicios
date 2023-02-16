240716656a8b71a315a28ec9d8949e2dd32d1bcc
Luiz Roberto <luizrobertoao29@gmail.com>
2023-02-16 17:06:22 -0300

    feat: Resolvido Ex2_Consoantes.

--- src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
+++ src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
@@ -8,4 +8,30 @@ Imprima as consoantes.
 import java.util.Scanner;
 
 public class Ex2_Consoantes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] letras = new String[6];
+        int totalConsoantes = 0;
+
+        for (int i = 0; i < letras.length; i++) {
+            System.out.println("Digite uma letra:");
+            String letra = sc.next();
+
+            boolean eConsoante = !(letra.equalsIgnoreCase("a") || letra.equalsIgnoreCase("e") ||
+                    letra.equalsIgnoreCase("i") || letra.equalsIgnoreCase("o")
+                    || letra.equalsIgnoreCase("u"));
+
+            if (eConsoante) {
+                totalConsoantes++;
+                letras[i] = letra;
+            }
+
+
+        }
+        System.out.println("Foram lidas " + totalConsoantes + " consoantes:");
+        for (int i = 0; i < letras.length; i++) {
+            if (letras[i] != null)
+                System.out.println(letras[i]);
+        }
+    }
 }
