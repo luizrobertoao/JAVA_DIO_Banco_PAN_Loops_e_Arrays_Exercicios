01e42f72be87283c9e9233521cd6c66b02b2189e
Luiz Roberto <luizrobertoao29@gmail.com>
2023-02-16 14:04:06 -0300

    feat: Resolvido Ex5_Tabuada.

--- src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -15,4 +15,23 @@ Tabuada de 5:
 5 X 10 = 50
 */
 public class Ex5_Tabuada {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero = 0;
+
+        do {
+            System.out.println("Digite um número para visualizar sua tabuada:");
+            numero = sc.nextInt();
+            if (!(numero <= 10 && numero >= 0)) {
+                System.out.println("Número inválido, digite um número entre 10 e 0.");
+            } else {
+                System.out.println("Tabuada de " + numero);
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(numero + " X " + i + " = " + (numero * i));
+                }
+            }
+        } while (!(numero <= 10 && numero >= 0));
+    }
 }
