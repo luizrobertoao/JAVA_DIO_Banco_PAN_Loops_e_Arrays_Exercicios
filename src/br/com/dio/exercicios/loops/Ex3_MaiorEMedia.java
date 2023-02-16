1eb5d5ad50aad405c1fd6869e94ce8670c67b006
Luiz Roberto <luizrobertoao29@gmail.com>
2023-02-16 12:12:37 -0300

    feat: Resolvido Ex3_MaiorEMedia.

--- src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
+++ src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
@@ -8,4 +8,22 @@ e informe o maior número
 e a média desses números.
 */
 public class Ex3_MaiorEMedia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroMaior = 0;
+        int soma = 0;
+        int media = 0;
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Digite um número: ");
+            int numero = sc.nextInt();
+            soma = (soma + numero);
+            if (numero > numeroMaior) numeroMaior = numero;
+            media = (soma / i);
+        }
+
+
+        System.out.println("O maior número digitado foi: " + numeroMaior);
+        System.out.println("A média dos números digitados é: " + media);
+    }
 }
